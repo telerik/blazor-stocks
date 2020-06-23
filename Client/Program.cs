077d4f9fb1c75512790c898e5168183695769dec
Marin Bratanov <m.bratanov@gmail.com>
2020-06-23 18:48:17 +0300

    chore: upgrade to WASM 3.2.0 and Telerik 2.14.1

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System;
 using BlazorFinancePortfolio.Services;
 using BlazorPro.BlazorSize;
 
@@ -15,7 +14,7 @@ namespace BlazorFinancePortfolio.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddTelerikBlazor();
             builder.Services.AddScoped<CurrenciesService>();
